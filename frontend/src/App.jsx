62eb3b0ac6e67c0bc95ab7c1d8167a0508ffab38
Adetola <hassannofisat017@gmail.com>
2023-08-01 22:34:53 +0100

    no styling yet

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import spotifyLogo from "./assets/spotify-logo.svg";
+
+function App() {
+  return (
+    <>
+      <div className="">
+        <img src={spotifyLogo} alt="Spotify Logo" />
+      </div>
+      <p>NOT CONNECTED</p>
+    </>
+  );
+}
+
+export default App;
